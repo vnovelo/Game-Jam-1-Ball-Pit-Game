442840a06ffa4f8393e8baca968eb3e6abc1b95a
Alex Novelo <vnovelo@asu.edu>
2019-01-28 23:21:10 -0700

    Updated game with Jump pads, Pickups, and UI

---
+++ Assets/Scripts/JumpPadManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadManager : MonoBehaviour
+{
+
+    public int jumpHeight;
+    private void OnCollisionEnter(Collision other)
+    {
+        //Causes the player to jump up to a certain height when stepping on the jump pad
+        if (other.gameObject.CompareTag("Player")) {
+            other.gameObject.GetComponent<Rigidbody>
+                ().AddForce(Vector3.up * jumpHeight);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup")){
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+
+        }
+    }
+
+    private void setCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
