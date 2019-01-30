4681d304630f7f63d5d2f6b091f7b460a451212e
agorowar <42878457+agorowar@users.noreply.github.com>
2019-01-30 16:13:16 -0700

    UI and Ball Spawner

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-    public GameObject player;
+	public GameObject player;
+	public float speed = 5.0f;
 
-    private Vector3 offset;
-    // Use this for initialization
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
+	private Vector3 offset;
+	private float inputX;
+	private float inputZ;
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
-    }
+	// Use this for initialization
+	void Start()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
 }
